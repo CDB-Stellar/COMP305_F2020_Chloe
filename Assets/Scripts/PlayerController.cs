93554700cd6cb30ebd642708b1e8455596aa4780
Chloe <chloe_baker@live.com>
2020-10-07 13:47:14 -0400

    Week 4
    - created character animations
    - updated player controller to send info for animations
    - flip player when facing left or right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -13,12 +15,15 @@ public class PlayerController : MonoBehaviour
 
     //Private Variables
     private Rigidbody2D rBody;
+    private Animator anim; //need this to be able to make transition parameters for the animtor. 
     private bool isGrounded = false;
+    private bool isFacingRight = true; //to flip animation, start off facing right
 
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Physics
@@ -36,13 +41,32 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
         }
 
-        rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y); 
+        rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
         //The first argument moves character left and right, the second argument helps with jumping, if you set it to 0 you can't jump
+
+        //Check if the sprite needs to be flipped:
+        if ((isFacingRight && rBody.velocity.x < 0) || (!isFacingRight && rBody.velocity.x > 0))
+            Flip();
+        //^ if player is facing right and the velocity turns negative, flip, or if player is facing left and velocity turns positive, flip.
+        //This could be an if else, but this works too
+
+        //Communicate with the animator:
+        anim.SetFloat("xVelocity", Mathf.Abs(rBody.velocity.x)); //use Mathf.Abs() to get the absolute value - you don't want to ignore moving left because it is negative
+        anim.SetFloat("yVelocity", rBody.velocity.y); //no absoulte value for jumping because JumpDown should be negative. 
+        anim.SetBool("IsGrounded", isGrounded); //pass this to IsGrounded parameter
     }
 
     private bool GroundCheck() //To jump, we will check first if player is on the ground. If they are then they have the ability to jump
     {
         //OverlapCircle return type is Collider2D which can easily be turned into a boolean
         return Physics2D.OverlapCircle(groundCheckPosition.position, groundCheckRadius, whatIsGround); //is character overlapping with an object that is ground?
+    }
+
+    private void Flip() //flip the animation when moving left vs moving right
+    {
+        Vector3 temp = transform.localScale; //this temp vector3 will allow you to flip it because it's not technically the game object
+        temp.x *= -1; //multiply by negative 1 to flip
+        transform.localScale = temp; //now make the object = temp to flip it haha
+        isFacingRight = !isFacingRight; //use this format of writing the bool so that no matter what it is it will turn the opposite way
     }
 }
