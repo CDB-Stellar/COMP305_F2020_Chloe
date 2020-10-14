9c90b72df2e03b25896b2550c64f0aa83e329ca8
Chloe <chloe_baker@live.com>
2020-10-14 17:06:07 -0400

    Week 5
    - working with cinemachine
    - created 3 types of cameras: one that follows similar to buffer camera, one that zooms in, target group camera
    - created 2 scripts:CameraTrigger and VirtualCameraController
    - added a sign to level 2
    - made the CameraRig prefab

---
+++ Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject cameraToActivate;
+    [SerializeField] private GameObject cameraOut;
+
+    public VirtualCameraController vCamController;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) //if you enter the bounding box - camera trigger
+            vCamController.TransitionTo(cameraToActivate); //using the method from VirtualCameraController
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) //if you exit the bounding box
+            vCamController.TransitionTo(cameraOut);
+    }
+}
