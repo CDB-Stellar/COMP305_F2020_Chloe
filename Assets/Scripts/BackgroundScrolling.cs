d04e39f729bff00ebeb7953e45669ef8cdc6984c
Chloe <chloe_baker@live.com>
2020-10-26 19:53:02 -0400

    Week 6
    - main menu created and start and quit functionality
    - scrolling background

---
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    private Renderer renderer; //making it of type renderer to be generic
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+
+        //2D aesthetic way
+        InvokeRepeating("MoveBG", scrollSpeed, scrollSpeed); //0.1 seconds after start it will happen, and every 0.1 seconds after that it will repeat
+    }
+
+    private void MoveBG()
+    {
+        //this is a more 2D aesthetic way of doing it
+        renderer.material.mainTextureOffset = new Vector2(renderer.material.mainTextureOffset.x + 0.05f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //renderer.material.mainTextureOffset = new Vector2(Time.time * scrollSpeed, 0); this is a smooth way to do it
+        //(time.time is a reference to the time since the start of the game, basing the scroll off that.)
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //to be able to work with scenes
+
+public class MainMenuController : MonoBehaviour
+{
+    public void NextLevel()
+    {
+        //SceneManager.LoadScene("MainMenu"); //argument can be the name of the scene
+        //SceneManager.LoadScene(0); //argument can be the build index
+        
+        //this will always go to the next in the build index whetever scene you're currently on
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        //Pre-processor directives will help us quit game once it is built or when we are still editing
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; //this will quit the game when we are editing
+#else
+        Application.Quit(); //This will quit the game but only once it is built
+#endif
+    }
+}
