35fb832e2d8253198c3514b8b165ee6377150202
Chloe <chloe_baker@live.com>
2020-11-04 21:58:27 -0500

    Week 8
    - added BGM 
    - added jump SFX
    - made audio listeners on the player and camera, learned how to make it on animation as well
    - created an audio mixer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,6 +39,7 @@ public class PlayerController : MonoBehaviour
         {
             rBody.AddForce(new Vector2(0f, jumpForce));
             isGrounded = false;
+            //GetComponent<AudioSource>().Play(); //this is one way to play an audio source
         }
 
         rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
