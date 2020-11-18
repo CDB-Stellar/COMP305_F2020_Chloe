ee33bc91149735b6b7aa2ccb22d405a31d2ced3f
Chloe <chloe_baker@live.com>
2020-11-18 17:54:20 -0500

    Week 9
    - created a moving platform that uses vector2 waypoints to travel between (no physics)
    - created a custom editor to show all the waypoint information
    - created a test scene to work with the platform
    - created 2 scripts: MovingPlatformController, and MovingPlatformControllerEditor

---
+++ Assets/Scripts/Editor/MovingPlatformControllerEditor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEditor; //access to editor stuff
+
+[CustomEditor(typeof(MovingPlatformController))] //accociate this script with MovingPlatformController script
+public class MovingPlatformControllerEditor : Editor //inherits Editor class
+{
+    public override void OnInspectorGUI() //the things drawn in the inspector box
+    {
+        MovingPlatformController controller = (MovingPlatformController)target; //the target is the instance of the MovingPlatformController script
+
+        // Draw some stuff in the editor
+        // Waypoint prefab 
+        controller.waypointPrefab = (GameObject)EditorGUILayout.ObjectField("Waypoint Object", controller.waypointPrefab, typeof(GameObject), false); //drag in the waypoint prefab to copy
+
+        // Speed value
+        controller.moveSpeed = EditorGUILayout.FloatField("Speed: ", controller.moveSpeed); //shows the speed value, and you can change it
+        
+        // Label for waypoints
+        EditorGUILayout.LabelField("Waypoints", EditorStyles.boldLabel);
+
+        //if there are waypoints, add them 
+        if (controller.waypoints != null && controller.waypoints.Count != 0) //it matters to check null before the count. 
+        {
+            for (int i = 0; i < controller.waypoints.Count; i++) //loop through waypoints
+            {
+                // Create horizontal groupings for each waypoint
+                EditorGUILayout.BeginHorizontal();
+
+                // Get the name of the game object for the label
+                controller.waypoints[i].gameObject.name = EditorGUILayout.TextField(controller.waypoints[i].gameObject.name);
+
+                controller.waypoints[i].position = EditorGUILayout.Vector2Field("", controller.waypoints[i].position);
+
+                // Add a button to delete waypoint
+                if (GUILayout.Button("Delete?")) //if you click on the button, perform action
+                {
+                    // Remove a specific waypoint (call controller for this)
+                    controller.RemoveWaypoint(i);
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+        // Button to add waypoint
+        if (GUILayout.Button("Add Waypoint")) //if you click on the button, perform action
+        {
+            // Add new waypoint to the waypoints list (tell the controller to add the waypoint)
+            controller.AddNewWaypoint();
+        }
+
+        // Button to clear waypoints
+        if (GUILayout.Button("Clear Waypoints"))
+        {
+            controller.ClearWaypoints(); //clears the list of waypoints
+        }
+    }
+}
---
+++ Assets/Scripts/MovingPlatformController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour
+{
+    // Will move the platform from a starting position to a waypoint. There may be multiple waypoints.
+    public GameObject waypointPrefab; //what to instatiate for the waypoint
+    public float moveSpeed = 5f;
+    public List<Transform> waypoints; //a list of waypoints
+
+    private int currentTargetIndex = 0; //keep track of the current waypoint index
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        waypoints = new List<Transform>(); //new list
+        //add already created waypoints incase they weren't deleted before
+        foreach (Transform t in transform.parent.GetChild(1)) //GetChild(1) = Waypoints empty object
+        {
+            waypoints.Add(t); //add the waypoint to the list
+        }
+
+        if (waypoints.Count > 0) //if there are any prexisting waypoints
+            transform.position = waypoints[0].position; //put the platform on the first position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waypoints.Count > 1) //if there is at least one waypoint start moving
+        {
+            // Move the object
+            // Going to manipulate the transform instead of using rigidbody.
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[currentTargetIndex].position, Time.deltaTime * moveSpeed); //current position, current waypoint, speed
+
+            if (Vector2.Distance(transform.position, waypoints[currentTargetIndex].position) < 0.01f) //if the platform gets very close to the target, true
+            {
+                // Change the target to the next
+                currentTargetIndex = (currentTargetIndex + 1) % waypoints.Count; //will loop through the list over and over!
+            }
+        }
+    }
+
+    public void AddNewWaypoint()
+    {
+        // Editor will call this
+        // Create a new waypoint from the prefab
+        GameObject newWaypoint = Instantiate(waypointPrefab, Vector2.zero, Quaternion.identity);
+        newWaypoint.transform.SetParent(transform.parent.GetChild(1)); //transform.parent.GetChild(1) = the Waypoints empty on the hierarchy
+        newWaypoint.name = "Waypoint " + waypoints.Count; //name the new object
+        waypoints.Add(newWaypoint.transform);
+    }
+
+    public void RemoveWaypoint(int index)
+    {
+        // Editor will call this
+        // Remove a waypoint from the list
+        waypoints.RemoveAt(index);
+        //waypoints.TrimExcess(); //recount the amount of things in list
+        DestroyImmediate(transform.parent.GetChild(1).GetChild(index).gameObject);
+    }
+
+    public void ClearWaypoints()
+    {
+        // Delete the waypoint game objects
+        for (int i = 0; i < waypoints.Count; i++)
+        {
+            DestroyImmediate(waypoints[i].gameObject);
+        }
+
+        waypoints.Clear(); //clears the list of waypoints
+    }
+}
